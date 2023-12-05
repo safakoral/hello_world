ece48c9b704061d3fbee0661fc7987e14408f2e3
safakoral <safakoral@gmail.com>
2023-12-05 23:09:26 +0300

    first_hello

--- hello_world.py
+++ hello_world.py
@@ -6,4 +6,4 @@ def hello_world():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="dev.project.com")
+    app.run(debug=True, host="dev.project.com",port="5000")
